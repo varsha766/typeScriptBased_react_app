a18db44828cedaab60a6863206f5e45cca874f94
varsha <varshakumari370@gmail.com>
2023-06-20 05:37:08 +0530

    added interface ITaskCounter

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -1,7 +1,9 @@
 import { Avatar, Box, Typography } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
-
-export const TaskCounter: FC = (): ReactElement => {
+import { ITaskCounter } from './interfaces/ITaskCounter';
+import { Status } from '../createTaskForm/enums/status';
+export const TaskCounter: FC<ITaskCounter> = (props): ReactElement => {
+  const { count = 0, status = Status.completed } = props;
   return (
     <>
       <Box
@@ -10,10 +12,26 @@ export const TaskCounter: FC = (): ReactElement => {
         justifyContent="center"
         alignItems="center"
       >
-        <Avatar>
-          <Typography>10</Typography>
+        <Avatar
+          sx={{
+            backgroundColor: 'transparent',
+            order: '5px solid',
+            width: '96px',
+            height: '96px',
+            marginBottom: '16px',
+            borderColor: 'warning',
+          }}
+        >
+          <Typography color="#ffffff">10</Typography>
         </Avatar>
-        <Typography>Subtitle</Typography>
+        <Typography
+          color="#ffffff"
+          fontWeight="bold"
+          fontSize="20px"
+          variant="h5"
+        >
+          Subtitle
+        </Typography>
       </Box>
     </>
   );
