668cc587065dccc23dd63a3fe70d33bcb4e20a65
varsha <varshakumari370@gmail.com>
2023-06-21 05:20:16 +0530

    added tanstack/react-query to link frontend and backend

--- projects/react/src/App.tsx
+++ projects/react/src/App.tsx
@@ -1,23 +1,32 @@
 import React, { FC, ReactElement } from 'react';
 // FC:- function componenet an interface used for functional components
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 // ThemeProvider: used to set custom theme
 // CssBaseline used to reset the basic value ased on the theme we passed
 import { customTheme } from './theme/customTheme';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { ReactQueryDevtools } from 'react-query/devtools';
 // function App() {
 //   return <h1>Hello World</h1>
 // }
 
 // we have converted function keyword declartion to arrow function
-//
-const App:FC = ():ReactElement => {
-  return (<ThemeProvider theme={customTheme}>
-    <CssBaseline>
-      <Dashboard/>
 
-    </CssBaseline>
-  </ThemeProvider>)
-}
+// Create a query client.Query clinet used make sync between frontend and backend
+const queryClient = new QueryClient();
+
+const App: FC = (): ReactElement => {
+  return (
+    // Incapsulating application within the query client provider
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline>
+          <Dashboard />
+        </CssBaseline>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
 export default App;
