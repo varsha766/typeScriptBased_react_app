6334a51f867913f57224be76a3d36e78aaba85a7
varsha <varshakumari370@gmail.com>
2023-06-21 06:22:53 +0530

    fired the post rquest on button click

--- projects/react/src/components/createTaskForm/createTaskForm.tsx
+++ projects/react/src/components/createTaskForm/createTaskForm.tsx
@@ -29,6 +29,19 @@ export const CreateTaskForm: FC = (): ReactElement => {
   const createTaskMutation = useMutation((data: ICreateTask) =>
     sendApiRequest('http://localhost:3200/tasks', 'POST', data),
   );
+  function createTaskHandler() {
+    if (!title || !date || !description) {
+      return;
+    }
+    const task: ICreateTask = {
+      title,
+      description,
+      date: date.toString(),
+      status,
+      priority,
+    };
+    createTaskMutation.mutate(task);
+  }
   return (
     <Box
       display="flex"
@@ -95,7 +108,12 @@ export const CreateTaskForm: FC = (): ReactElement => {
           />
         </Stack>
         <LinearProgress />
-        <Button variant="contained" size="large" fullWidth>
+        <Button
+          onClick={createTaskHandler}
+          variant="contained"
+          size="large"
+          fullWidth
+        >
           Create A Task
         </Button>
       </Stack>
